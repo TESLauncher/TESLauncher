513f5a79f06e62d2a3f92838c9b036ddf9de6f74
TheEntropyShard <theentropyshard@gmail.com>
2023-10-12 20:43:29 +0300

    Fixed GUI

--- src/main/java/me/theentropyshard/teslauncher/gui/Gui.java
+++ src/main/java/me/theentropyshard/teslauncher/gui/Gui.java
@@ -28,46 +28,46 @@ import javax.swing.plaf.ColorUIResource;
 import java.awt.*;
 
 public class Gui {
+    private JTabbedPane viewSelector;
     private AppWindow appWindow;
     private PlayView playView;
     private boolean darkTheme;
 
     public Gui(boolean darkTheme) {
         this.darkTheme = darkTheme;
+        this.initGui();
     }
 
     public void initGui() {
-        SwingUtilities.invokeLater(() -> {
-            if (this.darkTheme) {
-                UIManager.put("InstanceItem.defaultColor", new ColorUIResource(64, 75, 93));
-                UIManager.put("InstanceItem.hoveredColor", new ColorUIResource(70, 80, 100));
-                UIManager.put("InstanceItem.pressedColor", new ColorUIResource(60, 70, 86));
+        if (this.darkTheme) {
+            UIManager.put("InstanceItem.defaultColor", new ColorUIResource(64, 75, 93));
+            UIManager.put("InstanceItem.hoveredColor", new ColorUIResource(70, 80, 100));
+            UIManager.put("InstanceItem.pressedColor", new ColorUIResource(60, 70, 86));
 
-                UIManager.put("ProgressBar.selectionBackground", Color.WHITE);
-                UIManager.put("ProgressBar.selectionForeground", Color.WHITE);
+            UIManager.put("ProgressBar.selectionBackground", Color.WHITE);
+            UIManager.put("ProgressBar.selectionForeground", Color.WHITE);
 
-                FlatDarculaLaf.setup();
-            } else {
-                UIManager.put("InstanceItem.defaultColor", new ColorUIResource(222, 230, 237));
-                UIManager.put("InstanceItem.hoveredColor", new ColorUIResource(224, 234, 244));
-                UIManager.put("InstanceItem.pressedColor", new ColorUIResource(216, 224, 240));
+            FlatDarculaLaf.setup();
+        } else {
+            UIManager.put("InstanceItem.defaultColor", new ColorUIResource(222, 230, 237));
+            UIManager.put("InstanceItem.hoveredColor", new ColorUIResource(224, 234, 244));
+            UIManager.put("InstanceItem.pressedColor", new ColorUIResource(216, 224, 240));
 
-                UIManager.put("ProgressBar.selectionBackground", Color.BLACK);
-                UIManager.put("ProgressBar.selectionForeground", Color.BLACK);
+            UIManager.put("ProgressBar.selectionBackground", Color.BLACK);
+            UIManager.put("ProgressBar.selectionForeground", Color.BLACK);
 
-                FlatIntelliJLaf.setup();
-            }
+            FlatIntelliJLaf.setup();
+        }
 
-            JDialog.setDefaultLookAndFeelDecorated(true);
-            JFrame.setDefaultLookAndFeelDecorated(true);
-        });
+        JDialog.setDefaultLookAndFeelDecorated(true);
+        JFrame.setDefaultLookAndFeelDecorated(true);
+
+        this.viewSelector = new JTabbedPane(JTabbedPane.LEFT);
+        this.appWindow = new AppWindow(TESLauncher.TITLE, TESLauncher.WIDTH, TESLauncher.HEIGHT, this.viewSelector);
     }
 
     public void showGui() {
         SwingUtilities.invokeLater(() -> {
-            JTabbedPane viewSelector = new JTabbedPane(JTabbedPane.LEFT);
-            this.appWindow = new AppWindow(TESLauncher.TITLE, TESLauncher.WIDTH, TESLauncher.HEIGHT, viewSelector);
-
             this.playView = new PlayView();
             if (this.darkTheme) {
                 this.playView.getProgressBar().setForeground(new Color(64, 75, 93));
@@ -75,10 +75,10 @@ public class Gui {
                 this.playView.getProgressBar().setForeground(new Color(222, 230, 237));
             }
 
-            viewSelector.addTab("Play", this.playView.getRoot());
-            viewSelector.addTab("Accounts", new AccountsView().getRoot());
-            viewSelector.addTab("Settings", new SettingsView().getRoot());
-            viewSelector.addTab("About", new AboutView().getRoot());
+            this.viewSelector.addTab("Play", this.playView.getRoot());
+            this.viewSelector.addTab("Accounts", new AccountsView().getRoot());
+            this.viewSelector.addTab("Settings", new SettingsView().getRoot());
+            this.viewSelector.addTab("About", new AboutView().getRoot());
 
             this.appWindow.setVisible(true);
         });
